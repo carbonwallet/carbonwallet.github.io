fe3e036f7d060fffcbd5e8834257eadf2619f248
Aleksei Richards <alekseirichards@yahoo.com>
2014-03-12 15:30:44 +0100

    Seperate app from marketing

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     nodewebkit: {
       options: {
         build_dir: './build', // Where the build version of my node-webkit app is saved
-        credits: './public/credits.html',
+        credits: './app/credits.html',
         mac: true,
         win: false,
         linux32: false,
