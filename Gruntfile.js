703f1bdf9f052c3023469d374c1109003533051c
Aleksei Richards <alekseirichards@yahoo.com>
2014-03-12 13:51:23 +0100

    Little bit of node magic

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+ grunt.initConfig({
+    nodewebkit: {
+      options: {
+        build_dir: './build', // Where the build version of my node-webkit app is saved
+        credits: './public/credits.html',
+        mac: true,
+        win: false,
+        linux32: false,
+        linux64: false,
+      },
+      src: './**/*' // Your node-webkit app
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-node-webkit-builder');
+  grunt.registerTask('default', ['nodewebkit']);
+
+};
