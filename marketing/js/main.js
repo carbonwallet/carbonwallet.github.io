e4a4966cdcc156b1719781d2eb95a9dbfdfd0dfa
Aleksei Richards <alekseirichards@yahoo.com>
2014-03-19 13:33:01 +0100

    Above the fold is done.

--- marketing/js/main.js
+++ marketing/js/main.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
 
+  var userAgent = navigator.platform
 
+  var mac = /Mac/,
+      win = /Win/,
+      lin = /Lin/,
+      lin64 = /x86_64/;
+
+  if(mac.test(userAgent)) {
+  	document.getElementsByTagName('body')[0].className+=' mac';
+  } else if (win.test(userAgent)) {
+  	document.getElementsByTagName('body')[0].className+=' win';
+  } else if (lin.test(userAgent)) {
+    if(lin64.test(userAgent)) {
+      document.getElementsByTagName('body')[0].className+=' lin-64';
+    } else {
+      document.getElementsByTagName('body')[0].className+=' lin-32';
+    }
+  } else {
+  	document.getElementsByTagName('body')[0].className+=' nope';
+  }
 		
 })
